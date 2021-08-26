6a133bbf9853862be5614205b5e7ff5a5413aede
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-08-26 08:52:25 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='dda-rangequery',
+      version='1.0.1',
+      description='rangequery',
+      author='V.S.',
+      py_modules=['rangequery'],
+      install_requires=[
+         "pilton>=1.0.1",
+      ]
+     )
